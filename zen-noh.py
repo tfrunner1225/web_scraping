945eddbdc1c99035d4219d4adc969c5f38d531af
Teru <tfahlv@gmail.com>
2020-10-18 19:05:05 +0900

    add function save csv

--- zen-noh.py
+++ zen-noh.py
@@ -3,6 +3,8 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+import csv
+import datetime
 
 urlName = "https://www.tfm.co.jp/cdj/"             # URLを指定
 url = requests.get(urlName)                         # requestでWebページを取得
@@ -13,7 +15,27 @@ soup = BeautifulSoup(url.content, "html.parser")    # BeautifulSoupでHTMLを取
 elems = soup.find_all("div", {"class":["even cleafix", "odd cleafix"]})
 # print(elems)
 
+# elemの中からclass名（rank, song）のli要素を取得
+# songのliは"曲名 ／ アーティスト名"であるため分割して格納
+result = []
 for elem in elems:  
-    # print(elem)
     li = elem.find_all("li", {"class":{"rank","song"}})
-    print(li)
+    # print(li)
+    row = []
+    for l in li:
+        c = l.get("class").pop(0)
+        if c == "rank":
+            row.append(l.get_text())
+        elif c == "song":
+            t = l.get_text().split(" ／ ")
+            row = row + t
+    # print(row)
+    result.append(row)
+
+# 最終結果をcsvとして出力
+file = open('./web_scraping/zen-noh_{}.csv'.format(datetime.date.today()), 'w')
+w = csv.writer(file)
+w.writerows(result)
+file.close()
+# print(result)
+
