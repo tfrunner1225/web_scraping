73f08837471b021ae3478a32777100b87a0a6a7c
Teru <tfahlv@gmail.com>
2020-10-18 18:45:41 +0900

    add function get list

--- zen-noh.py
+++ zen-noh.py
@@ -8,3 +8,12 @@ urlName = "https://www.tfm.co.jp/cdj/"             # URLを指定
 url = requests.get(urlName)                         # requestでWebページを取得
 soup = BeautifulSoup(url.content, "html.parser")    # BeautifulSoupでHTMLを取得
 
+# div要素を取得して elems に格納
+# class名を"even cleafix", "odd cleafix"に指定
+elems = soup.find_all("div", {"class":["even cleafix", "odd cleafix"]})
+# print(elems)
+
+for elem in elems:  
+    # print(elem)
+    li = elem.find_all("li", {"class":{"rank","song"}})
+    print(li)
