a736e8ae7fa2b2e2b48a2f4959392c08e7ccc648
Teru <tfahlv@gmail.com>
2020-10-18 14:05:28 +0900

    Import libraries

---
+++ main.py
@@ -0,0 +1,11 @@
+# upgrade pip
+# pip3 install --upgrade pip
+
+# Install libraries before import
+# pip3 install requests
+# pip3 install BeautifulSoup4
+
+import requests
+from bs4 import BeautifulSoup
+import re
+
