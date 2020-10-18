a6c0f1632939f5e3a06b82b98adc2af0ce59cb2a
Teru <tfahlv@gmail.com>
2020-10-18 15:33:26 +0900

    add function get table from web page

--- fmfukuoka.py
+++ fmfukuoka.py
@@ -1,7 +1,25 @@
+# FM FUKUOKAのヒットチャートからデータを抽出する
+
 import requests
 from bs4 import BeautifulSoup
 import re
 
-urlName = "https://fmfukuoka.co.jp/hitchart/"
-url = requests.get(urlName)
-soup = BeautifulSoup(url.content, "html.parser")
+urlName = "https://fmfukuoka.co.jp/hitchart/"       # URLを指定
+url = requests.get(urlName)                         # requestでWebページを取得
+soup = BeautifulSoup(url.content, "html.parser")    # BeautifulSoupでHTMLを取得
+
+# tableを取得（class名を指定）
+table = soup.findAll("table", {"class":"table table-bordered hitchart_table"})[0]
+# 全ての行を取得
+rows = table.findAll("tr")
+
+result = []
+for row in rows:
+    dataRow = row.findAll(['td', 'th'])
+    resultRow = []
+    for cell in dataRow:
+        resultRow.append(cell.get_text())
+    # print(resultRow)
+    result.append(resultRow)
+
+print(result)
