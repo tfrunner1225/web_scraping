11d9ef67eda30021099cef66b3078c8fffcdca36
Teru <tfahlv@gmail.com>
2020-10-18 23:03:55 +0900

    get information of list

--- lovefm.py
+++ lovefm.py
@@ -14,14 +14,42 @@ soup = BeautifulSoup(url.content, "html.parser")    # BeautifulSoupでHTMLを取
 table = soup.findAll("table", {"class":"top40"})[0]
 rows = table.findAll("tr")
 
-for i in range(2):
+result = []
+for i in range(21):
     row = rows[i]
+    # class名を指定してtd要素を取得
     td = row.findAll("td", {"class":"music"})
+    # ヘッダーにはtdが含まれないので除外
     if len(td) != 0:
         # print(len(td))
         # print(td[0])
+
+        # 今週と先週の順位を取得
+        # 画像のファイル名を利用
+        imgs = row.findAll("img")
+        for img in imgs:
+            if img["src"].startswith("/img/top40/chart_lst"):
+                #print(img["src"])
+                s = img["src"].replace("/img/top40/chart_lst","")
+                last = s.replace(".gif","")
+            elif img["src"].startswith("/img/top/icon_new.gif"):
+                #print(img["src"])
+                last = "-"
+            elif img["src"].startswith("/img/top40/chart_"):
+                #print(img["src"])
+                s = img["src"].replace("/img/top40/chart_","")
+                this = s.replace(".gif","")
+            
+        print("This week rank is {}".format(this))
+        print("Last week rank is {}".format(last))
+
         [s, n] = td[0].findAll("li")
         song = s.get_text()
         name = n.get_text()
         print(song)
         print(name)
+
+        result.append([this,last,song,name])
+
+
+print(result)
